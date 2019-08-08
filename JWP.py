0e143352c09d1d06a3775b100e6ebae51342a46e
Philip <p208p2002@gmail.com>
2019-08-08 07:41:29 +0000

    no message

--- JWP.py
+++ JWP.py
@@ -8,7 +8,7 @@ d2vModel = Doc2Vec.load('d2vmodel/d2vmodel.model')
 POSTIVE_COMMENT_STRAT = 0
 NEGATIVE_COMMENT_START = 4000
 postiveAns = torch.ones([3000,1],dtype=torch.float)
-negativeAns = torch.ones([3000,1],dtype=torch.float)
+negativeAns = torch.zeros([3000,1],dtype=torch.float)
 
 postiveComments = []
 negativeComments = []
@@ -43,17 +43,26 @@ class JWP(nn.Module):
 net = JWP(200,150,1)
 print(net)
 
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.0002)
 loss_func = torch.nn.BCEWithLogitsLoss()  # the target label is NOT an one-hotted
 
-for t in range(100):
+
+#
+running_loss = 0.0
+running_acc = 0.0
+for t in range(250):
     # out = net(postiveComments)
     out = net(trainData)
-    # print('out',out)
+    # print(F.sigmoid(out[0]),trainDataAns[0],F.sigmoid(out[5000]),trainDataAns[5000])
     loss = loss_func(out,trainDataAns)
     loss.backward()
     optimizer.step()
-    print('loss',loss.data)
+    
+    loss_batch = loss.item()
+    running_loss += (loss_batch - running_loss) / (t+1)
+    print(loss.data,F.sigmoid(out[0]),trainDataAns[0],F.sigmoid(out[5000]),trainDataAns[5000])
+    # print('loss',)
+
     if(loss.data.numpy() <= 0.0001):
         break
     
